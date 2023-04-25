47f838d6f9357d494206163080cee3387e33b65a
amrizjantan <manager@edgedept.com>
2023-04-25 17:01:16 +0200

    header component created and added to App.vue
    Signed-off-by: amrizjantan <manager@edgedept.com>

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
-<script setup></script>
+<script setup>
+import Header from '@/components/Design/Default/Header.vue';
+</script>
 
 <template>
     <div class="w-screen h-screen">
+        <Header />
         <router-view></router-view>
     </div>
 </template>
---
+++ src/components/Design/Default/Header.vue
@@ -0,0 +1,36 @@
+<script setup></script>
+<template>
+    <div class="navbar bg-white h-36">
+        <div class="flex-1">
+            <a class="  " href="/"
+                ><img src="public/images/mrclou_logo-1.png" alt=""
+            /></a>
+        </div>
+        <div class="flex-none">
+            <ul class="menu menu-horizontal px-1">
+                <li><a class="uppercase">Home</a></li>
+                <li tabindex="0">
+                    <a class="uppercase">
+                        unser sortiment
+                        <svg
+                            class="fill-current"
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="20"
+                            height="20"
+                            viewBox="0 0 24 24"
+                        >
+                            <path
+                                d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"
+                            />
+                        </svg>
+                    </a>
+                    <ul class="p-2 bg-base-100">
+                        <li><a class="uppercase">säfte & mehr</a></li>
+                        <li><a class="uppercase">detox yourself</a></li>
+                    </ul>
+                </li>
+                <li><a class="uppercase">sepuerfoods</a></li>
+            </ul>
+        </div>
+    </div>
+</template>
